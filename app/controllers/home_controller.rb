52dd691f05aa3dc86954c8ba90945b267a816e7a
Pete Argent <pete@thecoderfactory.com>
2014-07-29 00:57:12 +1000

    fix ==

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
   	@info_enquiry = InfoEnquiry.new
   	@course_application = CourseApplication.new
   	@course = Course.find_by(name: 'Application Development')
-  	@intakes = Intake.future_intakes.where('course_id == ?', @course.id)
+  	@intakes = Intake.future_intakes.where('course_id = ?', @course.id)
   	if session[:ref]
   		@referrer = Referrer.find_by(referral_code: params[:ref])
   	end
